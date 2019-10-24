e484faa7cd56ca5576b201abb3a6812596145602
Alec Smecher <alec@smecher.bc.ca>
2019-10-24 12:52:26 -0700

    Release prep

--- JatsTemplatePlugin.inc.php
+++ JatsTemplatePlugin.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file JatsTemplatePlugin.inc.php
  *
- * Copyright (c) 2003-2018 Simon Fraser University
- * Copyright (c) 2003-2018 John Willinsky
+ * Copyright (c) 2003-2019 Simon Fraser University
+ * Copyright (c) 2003-2019 John Willinsky
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @brief JATS template plugin
