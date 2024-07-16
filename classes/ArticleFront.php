5d01ceec18bcbfdbb4a14123a1243476119efe59
Erik Hanson <erik@erikhanson.dev>
2024-07-16 11:32:26 -0700

    pkp/jatsTemplate#44 Display of multilingual keywords dependent on UI language

--- classes/ArticleFront.php
+++ classes/ArticleFront.php
@@ -285,7 +285,7 @@ class ArticleFront extends \DOMDocument
             ->setAttribute('xlink:href', $url);
 
         $submissionKeywordDao = DAORegistry::getDAO('SubmissionKeywordDAO');
-        foreach ($submissionKeywordDao->getKeywords($publication->getId(), $journal->getSupportedLocales()) as $locale => $keywords) {
+        foreach ($submissionKeywordDao->getKeywords($publication->getId(), $journal->getSupportedSubmissionLocales()) as $locale => $keywords) {
             if (empty($keywords)) continue;
 
             $kwdGroupElement = $articleMetaElement
@@ -413,7 +413,7 @@ class ArticleFront extends \DOMDocument
                 if (empty($bio)) {
                     continue;
                 }
-                
+
                 $bioElement = $this->createElement('bio');
                 $bioElement->setAttribute('xml:lang', substr($locale, 0, 2));
 
