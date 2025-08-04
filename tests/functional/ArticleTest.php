f21ac9304a753eee9fe219216252410919d17232
Touhidur Rahman <abir.ruet@gmail.com>
2025-08-04 13:08:43 +0600

    pkp/pkp-lib#11540 fixed SearchFileParser namespace and test files namespace path

--- tests/functional/ArticleTest.php
+++ tests/functional/ArticleTest.php
@@ -10,7 +10,7 @@
  * @brief JATS xml article unit tests
  */
 
-namespace APP\plugins\generic\jatsTemplate\functional;
+namespace APP\plugins\generic\jatsTemplate\tests\functional;
 
 use PKP\doi\Doi;
 use APP\issue\Issue;
